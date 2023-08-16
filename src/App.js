4930c0f9160edd6af50807c5338df303343721b4
smahamoud <115296163+smahamoud@users.noreply.github.com>
2023-08-16 22:25:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap'
 
 function App() {
   return (
